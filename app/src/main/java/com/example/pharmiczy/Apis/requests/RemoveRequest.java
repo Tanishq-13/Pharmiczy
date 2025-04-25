63713a93e0ff7bbbd06e1e0aafac717014d3bffb
Tanishq-13 <tanishqgupta326999@gmail.com>
2025-04-25 23:56:04 +0530

    Pharmiczy project-1st push

---
+++ app/src/main/java/com/example/pharmiczy/Apis/requests/RemoveRequest.java
@@ -0,0 +1,10 @@
+package com.example.pharmiczy.Apis.requests;
+
+public class RemoveRequest {
+    private String medicineId;
+//    private int quantity;
+
+    public RemoveRequest(String medicineId) {
+        this.medicineId = medicineId;
+    }
+}
